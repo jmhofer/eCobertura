65fbd9b62a495d5a45485626f5cc60d328fb091c
Joachim Hofer <joachim.hofer@johoop.de>
2009-09-27 14:13:16 +0200

    updated test to use new tracing

--- ecobertura.ui/test/ecobertura/ui/annotation/AnnotationModelAttacherTest.java
+++ ecobertura.ui/test/ecobertura/ui/annotation/AnnotationModelAttacherTest.java
@@ -1,8 +1,13 @@
 package ecobertura.ui.annotation;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.jface.text.source.IAnnotationModelExtension;
@@ -10,11 +15,15 @@ import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 // TODO define handler for logger and check warnings
 public class AnnotationModelAttacherTest {
 
 	abstract class AnnotationModelImpl implements IAnnotationModel, IAnnotationModelExtension {};
+	
+	private Handler handler;
+	private ArgumentCaptor<LogRecord> record;
 	
 	private ITextEditor editor;
 	private IDocumentProvider provider;
@@ -24,6 +33,18 @@ public class AnnotationModelAttacherTest {
 	
 	@Before
 	public void setUp() throws Exception {
+		registerTraceHandler();
+		mockEnvironment();
+	}
+
+	private void registerTraceHandler() {
+		handler = mock(Handler.class);
+		Logger.getLogger("ecobertura.ui").addHandler(handler);
+		Logger.getLogger("ecobertura.ui").setLevel(Level.ALL);
+		record = ArgumentCaptor.forClass(LogRecord.class);
+	}
+
+	private void mockEnvironment() {
 		editor = mock(ITextEditor.class);
 		provider = mock(IDocumentProvider.class);
 		invalidModel = mock(IAnnotationModel.class);
@@ -35,8 +56,15 @@ public class AnnotationModelAttacherTest {
 	public void testAttachToInvalidProvider() {
 		when(editor.getDocumentProvider()).thenReturn(null);
 		new AnnotationModelAttacher().attachTo(editor);
-//		verify(ilog, times(1)).log(status.capture());
-//		assertEquals("no document provider found", status.getValue().getMessage());
+		
+		verify(handler).publish(record.capture());
+		
+		LogRecord logged = record.getValue();
+		assertEquals("unable to attach to editors", logged.getMessage());
+		assertEquals(Level.WARNING, logged.getLevel());
+		Throwable thrown = logged.getThrown();
+		assertEquals("no document provider found", thrown.getMessage());
+		assertEquals(NoValidAnnotationModelException.class, thrown.getClass());
 	}
 	
 	@Test
@@ -44,8 +72,15 @@ public class AnnotationModelAttacherTest {
 		when(editor.getDocumentProvider()).thenReturn(provider);
 		when(provider.getAnnotationModel(any())).thenReturn(invalidModel);
 		new AnnotationModelAttacher().attachTo(editor);
-//		verify(ilog, times(1)).log(status.capture());
-//		assertTrue(status.getValue().getMessage().startsWith("unknown annotation model extension type"));
+		
+		verify(handler).publish(record.capture());
+		
+		LogRecord logged = record.getValue();
+		assertEquals("unable to attach to editors", logged.getMessage());
+		assertEquals(Level.WARNING, logged.getLevel());
+		Throwable thrown = logged.getThrown();
+		assertTrue(thrown.getMessage().startsWith("unknown annotation model extension type"));
+		assertEquals(NoValidAnnotationModelException.class, thrown.getClass());
 	}
 	
 	@Test
@@ -53,8 +88,12 @@ public class AnnotationModelAttacherTest {
 		when(editor.getDocumentProvider()).thenReturn(provider);
 		when(provider.getAnnotationModel(any())).thenReturn((IAnnotationModel) modelExt);
 		new AnnotationModelAttacher().attachTo(editor);
-//		verify(ilog, times(2)).log(status.capture());
-//		assertTrue(status.getValue().getMessage().startsWith("CoverageAnnotationModel attached to"));
+		
+		verify(handler, times(2)).publish(record.capture());
+		
+		LogRecord logged = record.getValue();
+		assertTrue(logged.getMessage().startsWith("CoverageAnnotationModel attached to"));
+		assertEquals(Level.FINE, logged.getLevel());
 	}
 
 	@Test
@@ -63,6 +102,6 @@ public class AnnotationModelAttacherTest {
 		when(provider.getAnnotationModel(any())).thenReturn((IAnnotationModel) modelExt);
 		when(modelExt.getAnnotationModel(CoverageAnnotationModel.MODEL_ID)).thenReturn(nonNullModel);
 		new AnnotationModelAttacher().attachTo(editor);
-//		verify(ilog, never()).log(any(IStatus.class));
+		verify(handler, never()).publish(any(LogRecord.class));
 	}
 }
