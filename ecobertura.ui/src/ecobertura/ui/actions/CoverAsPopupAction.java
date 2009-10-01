f8e0f0883bcd6ebef78895988b43188106c2f48b
Joachim Hofer <joachim.hofer@johoop.de>
2009-10-01 21:29:23 +0200

    added menu entries for "Cover As"

---
+++ ecobertura.ui/src/ecobertura/ui/actions/CoverAsPopupAction.java
@@ -0,0 +1,11 @@
+package ecobertura.ui.actions;
+
+import org.eclipse.debug.ui.actions.ContextualLaunchAction;
+
+public class CoverAsPopupAction extends ContextualLaunchAction {
+
+	public CoverAsPopupAction() {
+		super("ecobertura.core.coverageLaunchMode");
+	}
+
+}
