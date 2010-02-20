deb76f80eb6a08bec3cd2d3ccb41900dfa02d427
Joachim Hofer <joachim.hofer@johoop.de>
2010-02-20 14:37:22 +0100

    converted LaunchShortcutRetriever to Scala

---
+++ ecobertura.ui/src/ecobertura/ui/launching/LaunchShortcutRetriever.scala
@@ -0,0 +1,48 @@
+package ecobertura.ui.launching
+
+import java.util.logging._
+
+import org.eclipse.core.runtime._
+import org.eclipse.debug.ui._
+
+object LaunchShortcutRetriever {
+	private val logger = Logger getLogger "ecobertura.ui.launching" //$NON-NLS-1$
+	
+	def fromId(shortcutId: String): ILaunchShortcut = {
+		new LaunchShortcutRetriever(shortcutId).getShortcut
+	}
+}
+class LaunchShortcutRetriever(shortcutId: String) {
+	import LaunchShortcutRetriever._
+	
+	def getShortcut = {
+		def getShortcutFromExtensionPoint(launchShortcuts: IExtensionPoint) = {
+			val shortcut = launchShortcuts.getConfigurationElements find 
+					(shortcutId == _.getAttribute("id"))
+			shortcut match {
+				case Some(foundShortcutConfig) => 
+					getLaunchShortcutFromConfigurationElement(foundShortcutConfig)
+				case None => {
+					logger warning String.format(
+						"No standard launch shortcut named '%s' found.", shortcutId) //$NON-NLS-1$
+					null
+				}
+			}
+		}
+		
+		def getLaunchShortcutFromConfigurationElement(config: IConfigurationElement) = {
+			try {
+				config.createExecutableExtension("class").asInstanceOf[ILaunchShortcut] //$NON-NLS-1$
+			} catch {
+				case e: CoreException => {
+					logger.log(Level.WARNING, "unable to create launch shortcut object", e) //$NON-NLS-1$
+					null
+				}
+			}
+		}
+		
+		val launchShortcuts = Platform.getExtensionRegistry.getExtensionPoint(
+				IDebugUIConstants.PLUGIN_ID, IDebugUIConstants.EXTENSION_POINT_LAUNCH_SHORTCUTS)
+		getShortcutFromExtensionPoint(launchShortcuts)
+	}
+}
