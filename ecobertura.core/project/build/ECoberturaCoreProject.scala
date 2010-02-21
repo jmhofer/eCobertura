537b97b8b20e41db9797355ec6668c263716ea82
Joachim Hofer <joachim.hofer@johoop.de>
2010-02-21 15:22:23 +0100

    now at least the core compiles via sbt

--- ecobertura.core/project/build/ECoberturaCoreProject.scala
+++ ecobertura.core/project/build/ECoberturaCoreProject.scala
@@ -3,8 +3,12 @@ import scala.io
 import java.io.File
 
 class ECoberturaCoreProject(info: ProjectInfo) extends DefaultProject(info) {
-	def eclipseHome = 
-		Path.fromFile(new File(System.getenv("ECLIPSE_HOME")))
-		
-	def eclipsePlugins = descendents(eclipseHome, "*.jar")
+
+  def eclipseHome = Path.fromFile(new File(System.getenv("ECLIPSE_HOME")))
+  def eclipsePlugins = descendents(eclipseHome, "*.jar")
+
+  // add jars to "unmanaged" path 
+  // just including *all* eclipse plugin jars seems like a bit of a hack,
+  // but it works for now...
+  override def unmanagedClasspath = super.unmanagedClasspath +++ eclipsePlugins
 }
