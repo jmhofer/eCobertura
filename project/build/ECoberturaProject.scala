83ee45c57b9d5f0168ac7509a5f7cd601030adb9
Joachim Hofer <joachim.hofer@johoop.de>
2010-02-21 14:27:56 +0100

    added first beginnings of sbt build

---
+++ project/build/ECoberturaProject.scala
@@ -0,0 +1,14 @@
+import sbt._
+import scala.io
+import java.io.File
+
+class ECoberturaProject (info: ProjectInfo) extends ParentProject(info) {
+	def eclipseHome = 
+		Path.fromFile(new File(System.getenv("ECLIPSE_HOME")))	
+	def eclipsePlugins = descendents(eclipseHome, "*.jar")
+		
+//	override def localScala: Seq[ScalaInstance] = 
+//		defineScala("2.8.0-local", new File(eclipseHome + "/configuration/org.eclipse.osgi/bundles/623/1/.cp/lib"))
+	
+	lazy val core = project("ecoburtura.core", "eCobertura Core")
+}
