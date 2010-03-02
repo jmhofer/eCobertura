e37f4dd4aa3422b497a47c1157857873692f160e
Joachim Hofer <joachim.hofer@johoop.de>
2010-03-02 21:52:28 +0100

    trying to get sbt to create my ecobertura.ui package - it is working now, but i'm not sure if this is how it should work...

--- ecobertura.ui/project/build/ECoberturaUIProject.scala
+++ ecobertura.ui/project/build/ECoberturaUIProject.scala
@@ -1,8 +1,12 @@
 import sbt._
 import scala.io
-import java.io.File
+import java.io._
+import java.util.jar.Manifest
 
 class ECoberturaUIProject(info: ProjectInfo) extends DefaultProject(info) {
+
+  // dependencies
+  val ecobertura_core = "jmhofer" % "ecobertura-core_2.8.0.Beta1" % "1.0"
 
   def eclipseHome = Path.fromFile(new File(System.getenv("ECLIPSE_HOME")))
   def eclipsePlugins = descendents(eclipseHome, "*.jar")
@@ -11,9 +15,14 @@ class ECoberturaUIProject(info: ProjectInfo) extends DefaultProject(info) {
   // just including *all* eclipse plugin jars seems like a bit of a hack,
   // but it works for now...
   // TODO be more specific here
-  override def unmanagedClasspath = super.unmanagedClasspath +++ eclipsePlugins
+  override def unmanagedClasspath = super.unmanagedClasspath +++ eclipsePlugins +++ 
+      (Path.fromFile(new File("../ecobertura.core/lib")) * "*.jar")
+
+  // special eclipse plugin resources path
+  override def mainResources = ".options" +++ "build.properties" +++ "plugin.xml" +++ (("src" +++ "OSGI-INF" +++ "lib") ** "*")
 
   // TODO package needs to include our own manifest instead of the generated one, and also
   // needs a specific resources path
-  //override def packageOptions = JarManifest(new Manifest(new FileInputStream("src/main/resources/META-INF/MANIFEST.MF"))) :: Nil
+  override def packageOptions = JarManifest(new Manifest(new FileInputStream("META-INF/MANIFEST.MF"))) :: Nil
+
 }
