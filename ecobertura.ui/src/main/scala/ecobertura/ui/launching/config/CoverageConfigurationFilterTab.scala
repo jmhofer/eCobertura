446ef555c9c36affdff1574658e817e318675a17
Joachim Hofer <joachim.hofer@johoop.de>
2010-04-25 13:21:00 +0200

    Started adding a configuration tab card for coverage filters

---
+++ ecobertura.ui/src/main/scala/ecobertura/ui/launching/config/CoverageConfigurationFilterTab.scala
@@ -0,0 +1,44 @@
+/*
+ * This file is part of eCobertura.
+ * 
+ * Copyright (c) 2009, 2010 Joachim Hofer
+ * All rights reserved.
+ *
+ * eCobertura is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * eCobertura is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with eCobertura.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ecobertura.ui.launching.config
+
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTab
+import org.eclipse.debug.core._
+
+class CoverageConfigurationFilterTab extends AbstractLaunchConfigurationTab {
+  override def getName = "Filter Configuration"
+  
+  override def performApply(workingCopyOfLaunchConfiguration: ILaunchConfigurationWorkingCopy) = {
+    // TODO
+  }
+  
+  override def setDefaults(workingCopyOfLaunchConfiguration: ILaunchConfigurationWorkingCopy) = {
+    // TODO
+  }
+  
+  override def initializeFrom(launchConfiguration: ILaunchConfiguration) = {
+    // TODO
+  }
+  
+  override def createControl(parent: Composite) = {
+    // TODO
+  }
+}
