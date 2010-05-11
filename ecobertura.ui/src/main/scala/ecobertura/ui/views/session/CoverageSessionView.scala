72b5bda0abd2f98d072cf868bddc5a4635fae480
Joachim Hofer <joachim.hofer@johoop.de>
2010-05-11 20:23:01 +0200

    attempting to fix issue 4 (better cleanup and one more null check)

--- ecobertura.ui/src/main/scala/ecobertura/ui/views/session/CoverageSessionView.scala
+++ ecobertura.ui/src/main/scala/ecobertura/ui/views/session/CoverageSessionView.scala
@@ -37,98 +37,110 @@ import ecobertura.ui.util.Predef._
 import ecobertura.ui.views.session.labels._
 
 object CoverageSessionView {
-	/**
-	 * The ID of the view as specified by the extension.
-	 */
-	def ID = "ecobertura.ui.views.session.CoverageSessionView"
-		
-	private val logger = Logger.getLogger("ecobertura.ui.views.session") //$NON-NLS-1$
+  /**
+   * The ID of the view as specified by the extension.
+   */
+  def ID = "ecobertura.ui.views.session.CoverageSessionView"
+
+    private val logger = Logger.getLogger("ecobertura.ui.views.session") //$NON-NLS-1$
 }
 
 class CoverageSessionView extends ViewPart {
-	import CoverageSessionView.logger
-	
-	private var viewer: TreeViewer = null
+  import CoverageSessionView.logger
 
-	class NameSorter extends ViewerSorter
-	
-	/**
-	 * This is a callback that will allow us to create the viewer and initialize it.
-	 */
-	override def createPartControl(parent: Composite) = {
-		viewer = new TreeViewer(parent, SWT.SINGLE)
-		val swtTreeTable = viewer.getTree
-		swtTreeTable.setHeaderVisible(true)
-		swtTreeTable.setLinesVisible(true)
-		
-		registerContextMenu(swtTreeTable)
-		
-		val treeColumnLayout = new TreeColumnLayout
-		parent.setLayout(treeColumnLayout)
-		
-		addTreeColumn("Name", SWT.LEFT, 9).setLabelProvider(new NameLabelProvider)
-		addTreeColumn("Lines", SWT.RIGHT, 1).setLabelProvider(new LinesCoveredLabelProvider)
-		addTreeColumn("Total", SWT.RIGHT, 1).setLabelProvider(new LinesTotalLabelProvider)
-		addTreeColumn("%", SWT.RIGHT, 1).setLabelProvider(new LinesPercentageLabelProvider)
-		addTreeColumn("Branches", SWT.RIGHT, 1).setLabelProvider(new BranchesCoveredLabelProvider)
-		addTreeColumn("Total", SWT.RIGHT, 1).setLabelProvider(new BranchesTotalLabelProvider)
-		addTreeColumn("%", SWT.RIGHT, 1).setLabelProvider(new BranchesPercentageLabelProvider)
-		
-		viewer.setContentProvider(CoverageSessionModel.get)
-		viewer.setSorter(new NameSorter)
-		viewer.setInput(CoverageSessionRoot)
-		
-		viewer.addDoubleClickListener { 
-			event: DoubleClickEvent => {
-				val handlerService = getSite.getService(classOf[IHandlerService]).asInstanceOf[IHandlerService]
-				val ignore = handlerService.executeCommand(
-						"ecobertura.ui.views.session.commands.openCoveredClass", null)
-			}
-		}
-		
-		CoverageSessionModel.get.addSessionResetListener(coverageSession => {
-			logger.fine("Viewer has received sessionReset event")
-			viewer.setInput(CoverageSessionRoot)
+  private var viewer: TreeViewer = null
+  private var sessionResetListener: Option[Option[CoverageSession] => Unit] = None
+  
+  class NameSorter extends ViewerSorter
 
-			updateRadioStateOfRecentCoverageSessionMenu(coverageSession)
-		})
-
-		// Create the help context id for the viewer's control
-		// PlatformUI.getWorkbench.getHelpSystem.setHelp(viewer.getControl, "ecobertura.ui.viewer")
-
-		def addTreeColumn(name: String, alignment: Int, weight: Int) = {
-			val column = new TreeViewerColumn(viewer, alignment)
-			column.getColumn.setText(name)
-			column.getColumn.setAlignment(alignment)
-			treeColumnLayout.setColumnData(column.getColumn, new ColumnWeightData(weight))
-			
-			column
-		}
-
-		def updateRadioStateOfRecentCoverageSessionMenu(coverageSession: Option[CoverageSession]) = {
-			val commandService = getSite.getService(classOf[ICommandService]).asInstanceOf[ICommandService]
-			val command = commandService.getCommand("ecobertura.ui.views.session.commands.selectRecentCoverageSession")
-			HandlerUtil.updateRadioState(command, coverageSession match {
-				case Some(session) => session.displayName
-				case None => "undefined"
-			})
-		}
-	}
-
-	private def registerContextMenu(swtTreeTable: Tree) = {
-		val menuManager = new MenuManager
-		menuManager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS))
-		swtTreeTable.setMenu(menuManager.createContextMenu(swtTreeTable))
-		getSite.registerContextMenu("ecoburtura.ui.views.session.popup", menuManager, viewer)
-		getSite.setSelectionProvider(viewer)
-	}
-	
-	/**
-	 * Passing the focus request to the viewer's control.
-	 */
-	override def setFocus = {
-		viewer.getControl.setFocus
-	}
-	
-	def selection = viewer.getSelection
+  /**
+   * This is a callback that will allow us to create the viewer and initialize it.
+   */
+  override def createPartControl(parent: Composite) = {
+    viewer = new TreeViewer(parent, SWT.SINGLE)
+    val swtTreeTable = viewer.getTree
+    swtTreeTable.setHeaderVisible(true)
+    swtTreeTable.setLinesVisible(true)
+  
+    registerContextMenu(swtTreeTable)
+  
+    val treeColumnLayout = new TreeColumnLayout
+    parent.setLayout(treeColumnLayout)
+  
+    addTreeColumn("Name", SWT.LEFT, 9).setLabelProvider(new NameLabelProvider)
+    addTreeColumn("Lines", SWT.RIGHT, 1).setLabelProvider(new LinesCoveredLabelProvider)
+    addTreeColumn("Total", SWT.RIGHT, 1).setLabelProvider(new LinesTotalLabelProvider)
+    addTreeColumn("%", SWT.RIGHT, 1).setLabelProvider(new LinesPercentageLabelProvider)
+    addTreeColumn("Branches", SWT.RIGHT, 1).setLabelProvider(new BranchesCoveredLabelProvider)
+    addTreeColumn("Total", SWT.RIGHT, 1).setLabelProvider(new BranchesTotalLabelProvider)
+    addTreeColumn("%", SWT.RIGHT, 1).setLabelProvider(new BranchesPercentageLabelProvider)
+  
+    viewer.setContentProvider(CoverageSessionModel.get)
+    viewer.setSorter(new NameSorter)
+    viewer.setInput(CoverageSessionRoot)
+  
+    viewer.addDoubleClickListener { 
+      event: DoubleClickEvent => {
+        val handlerService = getSite.getService(classOf[IHandlerService]).asInstanceOf[IHandlerService]
+        val ignore = handlerService.executeCommand(
+            "ecobertura.ui.views.session.commands.openCoveredClass", null)
+      }
+    }
+  
+    sessionResetListener = Some((coverageSession: Option[CoverageSession]) => {
+      logger.fine("Viewer has received sessionReset event")
+      if (viewer.getContentProvider != null) {
+        viewer.setInput(CoverageSessionRoot)
+        updateRadioStateOfRecentCoverageSessionMenu(coverageSession)
+      } else {
+        logger.warning("attempted to reset session, but there is no content provider " +
+            "available")
+      }
+    })
+    
+    CoverageSessionModel.get.addSessionResetListener(sessionResetListener.get)
+  
+    // Create the help context id for the viewer's control
+    // PlatformUI.getWorkbench.getHelpSystem.setHelp(viewer.getControl, "ecobertura.ui.viewer")
+  
+    def addTreeColumn(name: String, alignment: Int, weight: Int) = {
+      val column = new TreeViewerColumn(viewer, alignment)
+      column.getColumn.setText(name)
+      column.getColumn.setAlignment(alignment)
+      treeColumnLayout.setColumnData(column.getColumn, new ColumnWeightData(weight))
+  
+      column
+    }
+  
+    def updateRadioStateOfRecentCoverageSessionMenu(coverageSession: Option[CoverageSession]) = {
+      val commandService = getSite.getService(classOf[ICommandService]).asInstanceOf[ICommandService]
+      val command = commandService.getCommand("ecobertura.ui.views.session.commands.selectRecentCoverageSession")
+      HandlerUtil.updateRadioState(command, coverageSession match {
+        case Some(session) => session.displayName
+        case None => "undefined"
+      })
+    }
+  }
+  
+  private def registerContextMenu(swtTreeTable: Tree) = {
+    val menuManager = new MenuManager
+    menuManager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS))
+    swtTreeTable.setMenu(menuManager.createContextMenu(swtTreeTable))
+    getSite.registerContextMenu("ecoburtura.ui.views.session.popup", menuManager, viewer)
+    getSite.setSelectionProvider(viewer)
+  }
+  
+  /**
+   * Passing the focus request to the viewer's control.
+   */
+  override def setFocus = {
+    viewer.getControl.setFocus
+  }
+  
+  override def dispose = sessionResetListener match {
+    case Some(listener) => CoverageSessionModel.get.removeSessionResetListener(listener)
+    case None => /* nothing to do */
+  }
+  
+  def selection = viewer.getSelection
 }
