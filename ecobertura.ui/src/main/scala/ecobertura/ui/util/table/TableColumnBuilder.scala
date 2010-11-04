dc936a885e072ddd95f3eaaf4c39895822daf324
jmhofer <none@none>
2010-11-04 20:22:28 +0100

    changed licence to EPL

--- ecobertura.ui/src/main/scala/ecobertura/ui/util/table/TableColumnBuilder.scala
+++ ecobertura.ui/src/main/scala/ecobertura/ui/util/table/TableColumnBuilder.scala
@@ -4,18 +4,10 @@
  * Copyright (c) 2010 Joachim Hofer
  * All rights reserved.
  *
- * eCobertura is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *  
- * eCobertura is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with eCobertura.  If not, see <http://www.gnu.org/licenses/>.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 package ecobertura.ui.util.table
 
